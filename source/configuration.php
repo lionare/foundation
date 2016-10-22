769e1650e3a8f6ced753d036fc409fc7e6066b8c
Aron Wouters <lionar@mail.com>
2016-10-22 20:02:58 +0200

    Added view to foundation.

--- source/configuration.php
+++ source/configuration.php
@@ -9,6 +9,8 @@ $configuration = new Configuration;
 $configuration->set ( 'file system disks', require configuration_path ( ) . '/file system/disks.php' );
 $configuration->set ( 'file system tree', require configuration_path ( ) . '/file system/tree.php' );
 
+$view = require configuration_path ( ) . '/view/view.php';
+$configuration->set ( 'theme path', $view [ 'path' ] );
 
 $application = $app;
 
--- source/providers.php
+++ source/providers.php
@@ -9,4 +9,5 @@
 return array (
 
 	'FileSystem\\ServiceProvider',
+	'View\\ServiceProvider',
 );
